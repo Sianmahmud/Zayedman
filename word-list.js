bd28525b0d6383821269acbf4e969260cdb7e765
Sianmahmud <sianmahmud92@gmail.com>
2024-12-25 01:47:16 +0600

    Add files via upload

---
+++ word-list.js
@@ -0,0 +1,30 @@
+const wordList = [
+    {
+        word: "guitar",
+        hint: "A musical instrument Zayed bhaiya is good at."
+    },
+    {
+        word: "black",
+        hint: "Zayed bhaiya's favourite color."
+    },
+    {
+        word: "hyperemesis",
+        hint: "Vomiting during pregnancy."
+    },
+    {
+        word: "faridpur",
+        hint: "A city located in Bangladesh."
+    },
+    {
+        word: "khichuri",
+        hint: "Zayed bhaiya's favourite dish."
+    },
+    {
+        word: "rahil",
+        hint: "Zayed bhaiya's biggest fan."
+    },
+    {
+        word: "zayed",
+        hint: "A chill guy."
+    },
+];
